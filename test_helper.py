b837c6aa1031a2e56f6817897822ec76ece91a53
WeAnnounce <weannounce@protonmail.com>
2020-04-09 20:33:30 +0200

    Add clear backends helper

--- test_helper.py
+++ test_helper.py
@@ -4,6 +4,9 @@ import test_backends
 async def announce(message):
     await test_irc.announce(message)
 
+def clear_all_backends():
+    return test_backends.clear_all_backends()
+
 def sonarr_received():
     return test_backends.sonarr_received()
 def radarr_received():
