c07a2c6c19cfd8afa4425778b90bfea28e11606e
WeAnnounce <weannounce@protonmail.com>
2020-04-11 10:08:02 +0200

    Cleanup run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -2,24 +2,9 @@
 
 import unittest
 import sys
-#import coverage
-
-#sys.path.append("helpers/")
-
-#cov = coverage.coverage(
-#    branch=True, include="src/*", omit=["*/__init__.py", "*/config/*"]
-#)
 
 if __name__ == "__main__":
-    #cov.start()
-
-    suite = unittest.TestLoader().discover("tests", pattern="single*")
+    suite = unittest.TestLoader().discover("tests", pattern="test_*.py")
     result = unittest.TextTestRunner().run(suite)
-
-    #cov.stop()
-    #cov.save()
-    #cov.report()
-    #cov.html_report(directory="./coverage")
-    #cov.erase
 
     sys.exit(0 if result.wasSuccessful() else 1)
