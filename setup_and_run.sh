47ff2edadabb319dbe57d09cf5205df4862d7013
WeAnnounce <weannounce@protonmail.com>
2020-04-29 16:12:22 +0200

    Start arrnounced in docker

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
 
+readonly container_name="arrnounced_test"
+readonly current_directory="$(readlink -f .)"
+readonly timezone="$(readlink -f /etc/localtime | sed -r 's#.*/(.*/.*)#\1#')"
+
 if [ ! -f "../arrnounced/src/arrnounced.py" ]
 then
   echo "Run script from test repo root"
   exit 1
 fi
 
-../arrnounced/src/arrnounced.py -v -c test_settings.cfg -d data -t trackers &
-trap 'kill %1' EXIT
+while getopts "d:" opt; do
+  case ${opt} in
+    d )
+      readonly docker_version="$OPTARG"
+      ;;
+    * )
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+if [ -n "$docker_version" ]
+then
+  echo "Starting docker..."
+  docker run --name "$container_name" \
+             --rm \
+             --net=host \
+             --user $(id -u) \
+             -d \
+             -v "$current_directory/data:/config" \
+             -v "$current_directory/trackers:/trackers" \
+             -e TZ="$timezone" \
+             -e VERBOSE=Y \
+             "weannounce/arrnounced:$docker_version"
+  trap "docker stop $container_name"  EXIT
+else
+  echo "Running from source..."
+  ../arrnounced/src/arrnounced.py -v -c test_settings.cfg -d data -t trackers &
+  trap 'kill %1' EXIT
+fi
 
 ./run_tests.py
