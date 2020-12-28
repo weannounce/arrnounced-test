5e35dc40df6dd3cd257ab1e41fc3ef853fd63e3b
WeAnnounce <weannounce@protonmail.com>
2020-12-28 10:13:10 +0100

    Add test for multiple backends of same type

---
+++ tests/test_double_backends.py
@@ -0,0 +1,246 @@
+import unittest
+from helpers import db, irc, backends, web, misc
+from helpers.misc import Release
+
+channel = "#simple1"
+config = misc.Config(
+    config_file="double_backends.toml",
+    irc_channels=[channel],
+    irc_users=["bipbopsimple1"],
+    backends=[
+        backends.Backend("Sonarr1", 10000),
+        backends.Backend("Sonarr2", 20000),
+        backends.Backend("Radarr1", 7878),
+        backends.Backend("Radarr2", 7879),
+        backends.Backend("Lidarr1", 5000),
+        backends.Backend("Lidarr2", 6000),
+    ],
+)
+
+
+class DelayTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        misc.setUpClass(config)
+
+    @classmethod
+    def tearDownClass(self):
+        misc.tearDownClass(config)
+
+    @db.db_session
+    def setUp(self):
+        backends.clear_all_backends()
+        db.clear_db()
+
+    def tearDown(self):
+        web.logout()
+
+    def test_no_snatch(self):
+        release = Release(
+            messages=["a title : some stuff"],
+            channel=channel,
+            title="a title",
+            url="smth: some stuff",
+            indexer="Simple1",
+            backends=[b.name for b in config.backends],
+        )
+
+        irc.announce(release)
+
+        for backend in [b.name for b in config.backends]:
+            backends.check_rx(
+                self, backend, release,
+            )
+
+        self.assertEqual(db.nr_announcements(), 1)
+        self.assertEqual(db.nr_snatches(), 0)
+
+        misc.check_announced(
+            self, config, release,
+        )
+
+    def test_radarr1_snatch(self):
+        release = Release(
+            messages=["two title : another stuff"],
+            channel=channel,
+            title="two title",
+            url="smth: another stuff",
+            indexer="Simple1",
+            backends=[b.name for b in config.backends],
+            snatches=["Radarr1"],
+        )
+
+        backends.send_approved("Radarr1", True)
+
+        irc.announce(release)
+
+        backends.check_rx(
+            self, "Radarr1", release,
+        )
+        backends.max_announcements(self, "Sonarr1", 1)
+        backends.max_announcements(self, "Sonarr2", 1)
+        backends.max_announcements(self, "Radarr2", 1)
+        backends.max_announcements(self, "Lidarr1", 1)
+        backends.max_announcements(self, "Lidarr2", 1)
+
+        self.assertEqual(db.nr_announcements(), 1)
+        self.assertEqual(db.nr_snatches(), 1)
+
+        misc.check_announced(
+            self, config, release,
+        )
+
+    def test_radarr2_snatch(self):
+        release = Release(
+            messages=["tree title : the stuff"],
+            channel=channel,
+            title="tree title",
+            url="smth: the stuff",
+            indexer="Simple1",
+            backends=[b.name for b in config.backends],
+            snatches=["Radarr2"],
+        )
+
+        backends.send_approved("Radarr2", True)
+
+        irc.announce(release)
+
+        backends.check_rx(
+            self, "Radarr2", release,
+        )
+        backends.max_announcements(self, "Sonarr1", 1)
+        backends.max_announcements(self, "Sonarr2", 1)
+        backends.max_announcements(self, "Radarr1", 1)
+        backends.max_announcements(self, "Lidarr1", 1)
+        backends.max_announcements(self, "Lidarr2", 1)
+
+        self.assertEqual(db.nr_announcements(), 1)
+        self.assertEqual(db.nr_snatches(), 1)
+
+        misc.check_announced(
+            self, config, release,
+        )
+
+    def test_sonarr1_snatch(self):
+        release = Release(
+            messages=["asdf : a stuff"],
+            channel=channel,
+            title="asdf",
+            url="smth: a stuff",
+            indexer="Simple1",
+            backends=[b.name for b in config.backends],
+            snatches=["Sonarr1"],
+        )
+
+        backends.send_approved("Sonarr1", True)
+
+        irc.announce(release)
+
+        backends.check_rx(
+            self, "Sonarr1", release,
+        )
+        backends.max_announcements(self, "Sonarr2", 1)
+        backends.max_announcements(self, "Radarr1", 1)
+        backends.max_announcements(self, "Radarr2", 1)
+        backends.max_announcements(self, "Lidarr1", 1)
+        backends.max_announcements(self, "Lidarr2", 1)
+
+        self.assertEqual(db.nr_announcements(), 1)
+        self.assertEqual(db.nr_snatches(), 1)
+
+        misc.check_announced(
+            self, config, release,
+        )
+
+    def test_sonarr2_snatch(self):
+        release = Release(
+            messages=["qwer : b stuff"],
+            channel=channel,
+            title="qwer",
+            url="smth: b stuff",
+            indexer="Simple1",
+            backends=[b.name for b in config.backends],
+            snatches=["Sonarr2"],
+        )
+
+        backends.send_approved("Sonarr2", True)
+
+        irc.announce(release)
+
+        backends.check_rx(
+            self, "Sonarr2", release,
+        )
+        backends.max_announcements(self, "Sonarr1", 1)
+        backends.max_announcements(self, "Radarr1", 1)
+        backends.max_announcements(self, "Radarr2", 1)
+        backends.max_announcements(self, "Lidarr1", 1)
+        backends.max_announcements(self, "Lidarr2", 1)
+
+        self.assertEqual(db.nr_announcements(), 1)
+        self.assertEqual(db.nr_snatches(), 1)
+
+        misc.check_announced(
+            self, config, release,
+        )
+
+    def test_lidarr1_snatch(self):
+        release = Release(
+            messages=["yuio : c stuff"],
+            channel=channel,
+            title="yuio",
+            url="smth: c stuff",
+            indexer="Simple1",
+            backends=[b.name for b in config.backends],
+            snatches=["Lidarr1"],
+        )
+
+        backends.send_approved("Lidarr1", True)
+
+        irc.announce(release)
+
+        backends.check_rx(
+            self, "Lidarr1", release,
+        )
+        backends.max_announcements(self, "Sonarr1", 1)
+        backends.max_announcements(self, "Sonarr2", 1)
+        backends.max_announcements(self, "Radarr1", 1)
+        backends.max_announcements(self, "Radarr2", 1)
+        backends.max_announcements(self, "Lidarr2", 1)
+
+        self.assertEqual(db.nr_announcements(), 1)
+        self.assertEqual(db.nr_snatches(), 1)
+
+        misc.check_announced(
+            self, config, release,
+        )
+
+    def test_lidarr2_snatch(self):
+        release = Release(
+            messages=["yuio : c stuff"],
+            channel=channel,
+            title="yuio",
+            url="smth: c stuff",
+            indexer="Simple1",
+            backends=[b.name for b in config.backends],
+            snatches=["Lidarr2"],
+        )
+
+        backends.send_approved("Lidarr2", True)
+
+        irc.announce(release)
+
+        backends.check_rx(
+            self, "Lidarr2", release,
+        )
+        backends.max_announcements(self, "Sonarr1", 1)
+        backends.max_announcements(self, "Sonarr2", 1)
+        backends.max_announcements(self, "Radarr1", 1)
+        backends.max_announcements(self, "Radarr2", 1)
+        backends.max_announcements(self, "Lidarr1", 1)
+
+        self.assertEqual(db.nr_announcements(), 1)
+        self.assertEqual(db.nr_snatches(), 1)
+
+        misc.check_announced(
+            self, config, release,
+        )
