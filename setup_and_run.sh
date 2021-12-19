396045ed959389d126aa3488e01ff14f32513e69
WeAnnounce <weannounce@protonmail.com>
2021-12-19 18:30:20 +0100

    Add py-spy
    Uncomment py-spy call in setup_and_run.py to produce a flamegraph

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -16,6 +16,7 @@ fi
 echo "Removing old log files"
 rm -vrf data/*.log*
 
+#py-spy record --idle -o profile.svg -- ./run_tests.py "$@" || RET=1
 ./run_tests.py "$@" || RET=1
 coverage combine
 coverage html
