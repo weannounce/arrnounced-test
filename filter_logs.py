0ece058ddf07064b8d444eaa3a0cccb6e480c92d
WeAnnounce <weannounce@protonmail.com>
2022-01-02 17:57:46 +0100

    Filter different logs on Python versions

--- filter_logs.py
+++ filter_logs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import fileinput
 import re
+import sys
 
 allowed_errors = [
     [
@@ -51,17 +52,6 @@ allowed_errors = [
     [r"WARNING:WEB-HANDLER *- Could not find the requested backend 'nonexistent'"],
     [r"WARNING:IRC:.+ - Unknown command: "],
     [
-        r"ERROR:SESSION\s+-\s+OS error when pushing release to",
-        r"Traceback .most recent call last.:",
-        r"raise exceptions.0.",
-        r"raise OSError.err, f'Connect call failed {address}'.",
-        r"ConnectionRefusedError: .Errno 111. Connect call failed .'127.0.0.1', 7880.",
-        r"The above exception was the direct cause of the following exception:",
-        r"Traceback .most recent call last.:",
-        r"raise client_error.req.connection_key, exc. from exc",
-        r"aiohttp.client_exceptions.ClientConnectorError: Cannot connect to host localhost:7880 ssl:default .Connect call failed .'127.0.0.1', 7880..",
-    ],
-    [
         r"ERROR:MESSAGE_HANDLER +- Database transaction failed for database transaction title",
         r"Traceback .most recent call last.",
         r"try: raise exc.with_traceback.tb.",
@@ -89,6 +79,36 @@ allowed_errors = [
         r"WARNING: This is a development server. Do not use it in a production deployment"
     ],
 ]
+
+if sys.version_info < (3, 8):
+    allowed_errors.append(
+        [
+            r"ERROR:SESSION\s+-\s+OS error when pushing release to http://localhost:7880",
+            r"Traceback .most recent call last.:",
+            r"raise exceptions.0.",
+            r"raise OSError.err, f'Connect call failed {address}'.",
+            r"ConnectionRefusedError: .Errno 111. Connect call failed .'127.0.0.1', 7880.",
+            r"The above exception was the direct cause of the following exception:",
+            r"Traceback .most recent call last.:",
+            r"client_error=client_error",
+            r"raise client_error.req.connection_key, exc. from exc",
+            r"aiohttp.client_exceptions.ClientConnectorError: Cannot connect to host localhost:7880 ssl:default .Connect call failed .'127.0.0.1', 7880..",
+        ]
+    )
+else:
+    allowed_errors.append(
+        [
+            r"ERROR:SESSION\s+-\s+OS error when pushing release to http://localhost:7880",
+            r"Traceback .most recent call last.:",
+            r"raise exceptions.0.",
+            r"raise OSError.err, f'Connect call failed {address}'.",
+            r"ConnectionRefusedError: .Errno 111. Connect call failed .'127.0.0.1', 7880.",
+            r"The above exception was the direct cause of the following exception:",
+            r"Traceback .most recent call last.:",
+            r"raise client_error.req.connection_key, exc. from exc",
+            r"aiohttp.client_exceptions.ClientConnectorError: Cannot connect to host localhost:7880 ssl:default .Connect call failed .'127.0.0.1', 7880..",
+        ]
+    )
 
 check_matched = [False for _ in allowed_errors]
 
